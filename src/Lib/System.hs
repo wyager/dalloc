c792e583f6a0db0cf7817e8bc0135c771534c5c4
Will Yager <will.yager@gmail.com>
2019-01-27 11:07:38 -0500

    Full exception coverage

--- src/Lib/System.hs
+++ src/Lib/System.hs
@@ -1,11 +1,11 @@
 module Lib.System  where
 
-import           Data.Store (Store, encode, decodeEx) 
-import           Control.Concurrent.Async (Async, async)
-import           Control.Concurrent (forkIO)
+import           Data.Store (Store, encode, decode, decodeEx, PeekException) 
+import           Control.Concurrent.Async (Async, async, link)
+-- import           Control.Concurrent (forkIO)
 import           Control.Concurrent.STM.TMVar (TMVar, newEmptyTMVarIO, takeTMVar, putTMVar)
 import           Control.Concurrent.STM.TVar (TVar)
-import           Control.DeepSeq (NFData, force)
+import           Control.DeepSeq (NFData, rnf, force)
 import           Control.Exception (evaluate)
 import           Control.Concurrent.STM.TBMChan (TBMChan, writeTBMChan)
 import           Control.Concurrent.STM.TBChan (TBChan, writeTBChan, readTBChan)
@@ -32,6 +32,9 @@ import           Control.Monad.Trans.Class (lift)
 import           Control.Monad.Morph (hoist)
 import           Numeric.Search.Range (searchFromTo)
 import           Numeric.Search (searchM, divForever, hiVal)
+-- import           Control.Monad.Error.Class (MonadError, throwError)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import           GHC.Exts (Constraint)
 
 newtype Segment = Segment Word64
     deriving newtype (Eq,Ord,Hashable, Store, Storable, Show)
@@ -59,13 +62,16 @@ data StoredOffsets = OffsetVector (VS.Vector Offset)
     deriving stock Generic
     deriving anyclass (Store, NFData)
 
-offset :: StoredOffsets -> Ix -> Offset
-offset (OffsetVector v) (Ix i) = v VS.! (fromIntegral i)
+
+data IndexError = IndexNotFound Ix
+
+offset :: Throws '[IndexError] m =>  StoredOffsets -> Ix -> m Offset
+offset (OffsetVector v) (Ix i) = return $ v VS.! (fromIntegral i)
 offset (SparseVector v) i = 
     case searchFromTo geq 0 (VS.length v - 1) of
         Just j | Assoc i' o <- v VS.! j,
-                 i' == i -> o
-        _ -> error "Index not found in stored offsets"
+                 i' == i -> return o
+        _ -> throw $ IndexNotFound i
     where
     geq j = let Assoc i' _o = (v VS.! j) in i' >= i
 
@@ -98,6 +104,7 @@ data WriteQueue = WriteQueue (TBMChan (WEnqueued Flushed (TMVar Ref)))
 
 data REnqueued = Read !Ref !(TMVar ByteString)
                | ReadComplete !Segment !MMapped
+               | ReadExn StoredOffsetsDecodeEx
                | SegmentWasGCed !Segment !MMapped -- The new segment (mmapped)
 
 data ReadQueue = ReadQueue (TBChan REnqueued)
@@ -112,10 +119,10 @@ storeToQueue (WriteQueue q) value = async $ do
     return (ref, flushed)
 
 
-mmap :: FilePath -> IO MMapped
+mmap :: (MonadIO m, Throws '[StoredOffsetsDecodeEx] m) => FilePath -> m MMapped
 mmap path = do
-    bs <- mmapFileByteString path Nothing
-    let offs = loadOffsets bs
+    bs <- liftIO $ mmapFileByteString path Nothing
+    offs <- either throw return $ loadOffsets bs
     return (MMapped offs bs)
 
 
@@ -137,14 +144,35 @@ emptySegmentCache :: Int -> SegmentCache
 emptySegmentCache maxSize = SegmentCache {openSegments = Lru.empty maxSize, openSegmentDetails = Map.empty}
 
 
-loadOffsets :: ByteString -> StoredOffsets
-loadOffsets bs | ByteString.length bs < 8 = error "Segment is too short to contain offset cache length tag"
-               | otherwise = 
-                    let final n = ByteString.drop (ByteString.length bs - n) bs in 
-                    let offsetLen = fromIntegral (decodeEx (final 8) :: Word64) in
-                    if | ByteString.length bs < 8 + offsetLen -> error "Segment is too short to contain purported offset cache"
-                       | otherwise -> let offsetsBs = ByteString.take offsetLen (final (offsetLen + 8)) in 
-                                      decodeEx offsetsBs :: StoredOffsets
+
+type family (a::k) ∈ (b::[k]) :: Constraint where
+    a ∈ (a ': xs) = ()
+    a ∈ (b ': xs) = a ∈ xs
+
+type family Throws (ts::[k]) (m :: * -> *) :: Constraint where
+    Throws '[] _ = ()
+    Throws (t ': ts) m = (MultiError t m, Throws ts m)
+
+
+data StoredOffsetsDecodeEx = CacheLengthDecodeEx PeekException | CacheDecodeEx PeekException | SegmentTooShortForCache
+
+instance NFData StoredOffsetsDecodeEx where
+    rnf (CacheLengthDecodeEx _) = ()
+    rnf (CacheDecodeEx _) = ()
+    rnf SegmentTooShortForCache = ()
+ 
+loadOffsets :: ByteString -> Either StoredOffsetsDecodeEx StoredOffsets
+loadOffsets bs = do
+    offsetCacheLen <- either (Left . CacheLengthDecodeEx) Right $ decode $ final 8 
+    let offsetCacheLen' = fromIntegral (offsetCacheLen :: Word64)
+    if ByteString.length bs < 8 + offsetCacheLen' 
+        then Left SegmentTooShortForCache
+        else do
+            let offsetsBs = ByteString.take offsetCacheLen' (final (offsetCacheLen' + 8)) 
+            either (Left . CacheDecodeEx) Right $ decode offsetsBs 
+    where
+    final n = ByteString.drop (ByteString.length bs - n) bs 
+    
 
 updateSegment :: Segment -> MMapped -> SegmentCache -> SegmentCache
 updateSegment segment mmapped cache@SegmentCache{..} = 
@@ -160,9 +188,12 @@ insertSegment segment mmapped SegmentCache{..} =  SegmentCache open' details'
 
 
 data Order = Asc | Desc
-streamSegFromOffsets :: Monad m => Order -> MMapped -> Stream (Of (Ix, ByteString)) m ()
-streamSegFromOffsets order (MMapped stored bs) = mapM_ (\(ix,off) -> yield (ix, readOff bs off)) offsets
+streamSegFromOffsets :: Throws '[OffsetDecodeEx] m => Order -> MMapped -> Stream (Of (Ix, ByteString)) m ()
+streamSegFromOffsets order (MMapped stored bs) = mapM_ load offsets
     where 
+    load (ix,off) = do
+        val <- lift $ readOff bs off
+        yield (ix, val)
     offsets = case order of
         Asc -> inorder
         Desc -> reverse inorder -- Can optimize this whole thing to remove list, if needed
@@ -171,35 +202,37 @@ streamSegFromOffsets order (MMapped stored bs) = mapM_ (\(ix,off) -> yield (ix, 
         SparseVector v -> fmap (\(Assoc ix off) -> (ix,off)) $ VS.toList v
 
 
+data OffsetDecodeEx = OffsetDecodeEx PeekException
 
-readOff :: ByteString -> Offset ->  ByteString
-readOff bs (Offset theOffset) = value
-    where
-    len :: Word64 = decodeEx $ ByteString.take 8 $ ByteString.drop (fromIntegral theOffset) bs
-    value = ByteString.take (fromIntegral len) $ ByteString.drop (fromIntegral theOffset + 8) bs
+readOff :: Throws '[OffsetDecodeEx] m => ByteString -> Offset -> m ByteString
+readOff bs (Offset theOffset) = do
+        len :: Word64 <- either (throw . OffsetDecodeEx) return $ decode $ ByteString.take 8 $ ByteString.drop (fromIntegral theOffset) bs 
+        return $ ByteString.take (fromIntegral len) $ ByteString.drop (fromIntegral theOffset + 8) bs
 
-readSeg :: MMapped-> Ix -> ByteString
-readSeg (MMapped offs bs) ix = readOff bs (offset offs ix) 
+readSeg :: (Throws '[IndexError, OffsetDecodeEx] m) => MMapped-> Ix -> m ByteString
+readSeg (MMapped offs bs) ix = readOff bs =<< offset offs ix
 
     
 
-readSegCache :: SegmentCache -> Ref -> Maybe (ByteString, SegmentCache)
+data CacheConsistencyError = SegmentInDetailsButNotLRU Segment
+
+readSegCache :: (Throws [IndexError, CacheConsistencyError, OffsetDecodeEx] m) => SegmentCache -> Ref -> m (Maybe (ByteString, SegmentCache))
 readSegCache cache@SegmentCache{..} (Ref seg ix) = 
     case Map.lookup seg openSegmentDetails of
-        Nothing -> Nothing
-        Just mmapped -> Just (value, cache {openSegments = open'})
-            where
-            value = readSeg mmapped ix
-            open' = case Lru.lookup seg openSegments of
-                Just ((), new) -> new
-                Nothing -> error "A cache segment is present in the details map, but not the open segment LRU"
+        Nothing -> return Nothing
+        Just mmapped -> do
+            value <- readSeg mmapped ix
+            open' <- case Lru.lookup seg openSegments of
+                Just ((), new) -> return new
+                Nothing -> throw $ SegmentInDetailsButNotLRU seg
+            return $ Just (value, cache {openSegments = open'})
 
 
 data ReaderState = ReaderState {
     cached :: LruCache Ref ByteString,
     segmentCache :: SegmentCache,
     pending :: Map Segment (Map Ix [TMVar ByteString])
-}
+}  
 
 data Found a = NoK1 | NoK2 | Found a
 
@@ -217,56 +250,65 @@ appsert f k1 k2 m = (m', found)
     g (Just inner) = Just (Map.insert k2 a' inner)
 
 
-
-
-reader :: ReaderConfig -> ReadQueue -> IO void
+reader :: (MonadIO m, Throws '[IndexError, CacheConsistencyError, OffsetDecodeEx, ReaderConsistencyError, StoredOffsetsDecodeEx] m) => ReaderConfig -> ReadQueue -> m void
 reader ReaderConfig{..} (ReadQueue q) = go initial
     where
     initial = ReaderState (Lru.empty lruSize) (emptySegmentCache maxOpenSegs) Map.empty
     go state = do
-        enqueued <- atomically $ readTBChan q
+        enqueued <- liftIO $ atomically $ readTBChan q
         (state', toDispatch) <- readerStep state enqueued
         case toDispatch of
             Nothing -> return ()
-            Just seg -> do
-                _tid <- forkIO $ do
+            Just seg -> liftIO $ do
+                serializer <- async $ do
                     bs <- openSeg seg
-                    offsets <- evaluate $ force $ loadOffsets bs
-                    atomically $ writeTBChan q (ReadComplete seg $ MMapped offsets bs)
-                return ()
+                    loaded <- evaluate $ force $ loadOffsets bs
+                    let toSend = case loaded of
+                            Right offsets -> ReadComplete seg $ MMapped offsets bs
+                            Left exn -> ReadExn exn
+                    atomically $ writeTBChan q toSend 
+                link serializer -- Should be exn-free, but just in case
         go state'
+        
 
 
-readerStep :: ReaderState -> REnqueued -> IO (ReaderState, Maybe Segment)
+data ReaderConsistencyError = ReadCompletedButNoOneCared Segment
+
+readerStep :: (MonadIO m, Throws '[IndexError, OffsetDecodeEx, StoredOffsetsDecodeEx, CacheConsistencyError, ReaderConsistencyError] m) => ReaderState -> REnqueued -> m (ReaderState, Maybe Segment)
 readerStep state@ReaderState{..} = \case
     Read ref@(Ref seg ix) done -> case Lru.lookup ref cached of
         Just (string, cached') -> do
-            atomically (putTMVar done string)
+            liftIO $ atomically (putTMVar done string)
             return (state {cached = cached'}, Nothing)
-        Nothing -> case readSegCache segmentCache ref of
-            Just (string, cache') -> do
-                atomically (putTMVar done string)
-                return (state {segmentCache = cache'}, Nothing)
-            Nothing -> 
-                let (pending', waiting) = appsert (maybe [done] (done:)) seg ix pending in
-                case waiting of
-                    NoK1    -> return (state {pending = pending'}, Just seg)
-                    NoK2    -> return (state {pending = pending'}, Nothing)
-                    Found _ -> return (state {pending = pending'}, Nothing)
+        Nothing -> do
+            cachedRef <- readSegCache segmentCache ref
+            case cachedRef of
+                Just (string, cache') -> do
+                    liftIO $ atomically (putTMVar done string)
+                    return (state {segmentCache = cache'}, Nothing)
+                Nothing -> do
+                    let (pending', waiting) = appsert (maybe [done] (done:)) seg ix pending
+                        state' = state {pending = pending'}
+                        toDispatch = case waiting of
+                            NoK1    -> Just seg
+                            NoK2    -> Nothing
+                            Found _ -> Nothing
+                    return (state', toDispatch)
     ReadComplete  seg mmapped -> 
         let remove _k _v = Nothing in
         case Map.updateLookupWithKey remove seg pending of
-            (Nothing, _) -> error "Pending read completed, but no one was waiting for it"
+            (Nothing, _) -> throw $ ReadCompletedButNoOneCared seg
             (Just waiting, pending') -> do
                 let cache' = insertSegment seg mmapped segmentCache
-                    clear ix dones = 
-                        let value = readSeg mmapped ix in 
-                        mapM_ (\done -> atomically (putTMVar done value)) dones
+                    clear ix dones = do
+                        value <- readSeg mmapped ix 
+                        mapM_ (\done -> liftIO $ atomically $ putTMVar done value) dones
                 _ <- Map.traverseWithKey clear waiting
                 return (state {pending = pending', segmentCache = cache'}, Nothing)
     SegmentWasGCed seg mmapped -> 
         let cache' = updateSegment seg mmapped segmentCache in 
         return (state {segmentCache = cache'}, Nothing)
+    ReadExn exn -> throw exn
 
 data FilenameConfig = FilenameConfig {
         segmentPath :: Segment -> FilePath,
@@ -274,7 +316,7 @@ data FilenameConfig = FilenameConfig {
     }
 
 
-data InitResult = NoData | CleanShutdown Segment | AbruptShutdown Segment
+data InitResult = NoData | CleanShutdown Segment | AbruptShutdown Segment | IDon'tUnderstandSearchM
 
 initialize :: FilenameConfig -> IO InitResult
 initialize FilenameConfig{..} = do
@@ -289,7 +331,7 @@ initialize FilenameConfig{..} = do
             doesFileExist (partialSegmentPath (Segment (highestExists + 1))) >>= \case
                 True -> return $ AbruptShutdown (Segment (highestExists + 1))
                 False -> return $ CleanShutdown (Segment highestExists)
-        _ -> error "I don't understand how searchM works"
+        _ -> return IDon'tUnderstandSearchM
 
 
 data CoreState = CoreState {
@@ -356,28 +398,31 @@ data SegGcState = SegGcState {
 }
 
 
-gc :: GcConfig ByteString -> Set Ix -> Handle -> MMapped -> IO (StoredOffsets, Set Ref)
+
+gc :: (Throws '[GCError, OffsetDecodeEx] m, MonadIO m) => GcConfig ByteString -> Set Ix -> Handle -> MMapped -> m (StoredOffsets, Set Ref)
 gc cfg live newFile = fmap unwrap . repackFile newFile . gc' cfg live . streamSegFromOffsets Desc
     where unwrap (offs :> live' :> ()) = (offs, live')
 
 
+data GCError = GCDeserializeError | SegmentCausalityViolation Segment Ref | IndexCausalityViolation Ix Ref
 
-gc' :: Monad m => GcConfig entry -> Set Ix -> Stream (Of (Ix, entry)) m o -> Stream (Of (Ix, entry)) m (Of (Set Ref) o)
+
+gc' :: Throws '[GCError] m => GcConfig entry -> Set Ix -> Stream (Of (Ix, entry)) m o -> Stream (Of (Ix, entry)) m (Of (Set Ref) o)
 gc' GcConfig{..} here = foldM include (return (SegGcState here Set.empty)) (return . liveThere) . hoist lift
     where
     include state (ix,bs) = if ix `Set.member` (liveHere state)
                             then do
                                 yield (ix,bs)
-                                return (includeChildren state ix bs)
+                                includeChildren state ix bs
                             else return state
-    includeChildren state thisIx bs = VS.foldl' step state children
+    includeChildren state thisIx bs = VS.foldM' step state children
         where
         children = childrenOf bs
         step s@SegGcState{..} ref@(Ref seg ix)
-            | seg > thisSegment = error "Segment causality violation"
-            | seg == thisSegment && ix >= thisIx = error "Index causality violation"
-            | seg == thisSegment = s {liveHere = Set.insert ix liveHere}
-            | otherwise = s {liveThere = Set.insert ref liveThere}
+            | seg > thisSegment = lift $ throw $ SegmentCausalityViolation thisSegment ref 
+            | seg == thisSegment && ix >= thisIx = lift $ throw $ IndexCausalityViolation thisIx ref 
+            | seg == thisSegment = return $  s {liveHere = Set.insert ix liveHere}
+            | otherwise = return $ s {liveThere = Set.insert ref liveThere}
 
 
 data RepackState = RepackState {
@@ -385,16 +430,16 @@ data RepackState = RepackState {
         repackMap :: Map Ix Offset
     }
 
-repackFile :: Handle -> Stream (Of (Ix, ByteString)) IO r -> IO (Of StoredOffsets r)
+repackFile :: MonadIO m => Handle -> Stream (Of (Ix, ByteString)) m r -> m (Of StoredOffsets r)
 repackFile hdl = foldM step (return initial) finalize
     where
     step RepackState{..} (ix,bs) = do
-        ByteString.hPut hdl bs
+        liftIO $ ByteString.hPut hdl bs
         return (RepackState {repackOffset = repackOffset `plus` ByteString.length bs,
                              repackMap = Map.insert ix repackOffset repackMap})
     finalize RepackState{..} = do
         let offsets = SparseVector $ VS.fromList $ fmap (uncurry Assoc) $ Map.toList repackMap
-        writeOffsetsTo hdl offsets
+        liftIO $ writeOffsetsTo hdl offsets
         return offsets
     initial = RepackState {repackOffset = Offset 0, repackMap = Map.empty}
 
@@ -413,6 +458,10 @@ data ConsumerState flushed = ConsumerState {
 
 
 data SegmentStep = Incomplete | Finalizer ByteString | Packet ByteString ByteString
+
+
+
+
 segmentStep :: ByteString -> SegmentStep
 segmentStep bs = 
     if | totalLen < 8 -> Incomplete
@@ -426,7 +475,15 @@ segmentStep bs =
     afterLen = ByteString.drop 8 bs
     (packet,remainder) = ByteString.splitAt packetLen' afterLen
 
-stream :: Monad m => ByteString -> Stream (Of ByteString) m (Maybe ByteString)
+
+data SegmentStreamError = RepeatedFinalizer
+
+-- no fundep m -> e
+class Monad m => MultiError e m where
+    throw :: e -> m a
+
+
+stream :: Throws '[SegmentStreamError] m => ByteString -> Stream (Of ByteString) m (Maybe ByteString)
 stream = go
     where
     go bs = case segmentStep bs of
@@ -434,10 +491,11 @@ stream = go
         Finalizer next -> case segmentStep next of
             Packet packet _ -> return (Just packet)
             Incomplete -> return Nothing
-            Finalizer _ -> error "Repeated finalizer"
+            Finalizer _ -> lift $ throw RepeatedFinalizer
         Packet packet next -> do
             yield packet
             go next
+
 
 
 data ConsumerAction flushed ref = Flush [flushed] | WriteToLog ByteString (Map Offset ByteString) ref Ix | Nop
