2462ded001c46a8d2bc5bcd74016e18d51312145
will <will@yager.io>
2019-09-07 12:18:48 +0800

    benhc

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -24,13 +24,13 @@ main = CM.runMode (CMO.Run CM.defaultConfig CMO.Prefix [""]) [ff 16 32]
     )
   withFF ff@(G.FillFactor _min max_) =
     [ addUp ff
-    -- , CM.bench "insert max elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) max_
-    -- , CM.bench "insert (max+1) elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) (max_ + 1)
-    -- , CM.bench "insert 100 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 100
+    , CM.bench "insert max elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) max_
+    , CM.bench "insert (max+1) elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) (max_ + 1)
+    , CM.bench "insert 100 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 100
     , CM.bench "insert 1,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 1000
-    -- , CM.bench "insert 10,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 10000
-    -- , CM.bench "insert 100,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 100000
-    -- , CM.bench "insert 1,000,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 1000000
+    , CM.bench "insert 10,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 10000
+    , CM.bench "insert 100,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 100000
+    , CM.bench "insert 1,000,000 elements into Identity-backed GiST" $ CM.nf (Gex.bigSet ff) 1000000
     , CM.bench "insert 1,000 elements into In-memory Database backed GiST" $ CM.nfIO (dbTestMem ff 1000)
     , CM.bench "insert 1,000 elements into In-memory Database backed GiST" $ CM.nfIO (dbTestDisk ff 1000)
     ]
